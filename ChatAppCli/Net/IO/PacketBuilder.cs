9c40ad081ed6167e82fb3a47522247c28c38c415
Opilane TTHK <opilane@tthk.ee>
2024-10-24 12:34:05 +0300

    update

---
+++ ChatAppCli/Net/IO/PacketBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ChatAppCli.Net.IO;
+
+public class PacketBuilder
+{
+    private MemoryStream _ms;
+    public PacketBuilder()
+    {
+        _ms = new MemoryStream();
+    }
+
+    public void WriteOpCode(byte opcode)
+    {
+        _ms.WriteByte(opcode);
+    }
+
+    public void WriteMessage(string msg)
+    {
+        var msgBytes = Encoding.ASCII.GetBytes(msg);
+        var msgLength = msgBytes.Length;
+        _ms.Write(BitConverter.GetBytes(msgLength));
+        _ms.Write(msgBytes);
+    }
+
+
+    public byte[] GetPacketBytes()
+    {
+        return _ms.ToArray();
+    }
+
+}
--- ChatAppCli/Program.cs
+++ ChatAppCli/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChatAppCli.Net.IO;
+using ChatAppCli;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+
+string name;
+string message;
+
+Console.Write("Nimi: ");
+name = Console.ReadLine();
+
+Server server = new Server();
+server.ConnectToServer(name);
+
+Console.Write("Message: ");
+message = Console.ReadLine();
+
+server.SendMessageToServer(message);
+Console.WriteLine(name + " sent message: " + message);
+
+// If you want to keep the console open until the user presses a key
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
